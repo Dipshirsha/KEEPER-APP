c00f26e150044b20cabce4c10438c28b01cf66a0
Dipshirsha <140639876+Dipshirsha@users.noreply.github.com>
2023-12-13 20:43:36 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const currentyear=new Date().getFullYear();
+
+function Footer(){
+    return(
+        <footer>
+         
+    Copyright @ {currentyear}
+
+        </footer>
+
+
+    );
+}
+
+export default Footer;
